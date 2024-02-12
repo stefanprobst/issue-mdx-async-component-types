c8c83537e0ceb784047893e011f55c781d8dc7d6
Stefan Probst <stefanprobst@posteo.de>
2024-02-12 13:21:54 +0100

    don't use experimental mdx/rs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
-  experimental: {
-    mdxRs: true,
-  },
+  // typescript: { ignoreBuildErrors: true },
 };
 
 const withMDX = require("@next/mdx")();
+
 module.exports = withMDX(nextConfig);
